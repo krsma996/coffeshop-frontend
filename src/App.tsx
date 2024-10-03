7eaea4c9575403e50b7258bbec8074933a8d2ceb
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2024-10-03 19:34:42 +0200

    removed usless components
    removed useless components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { DeleteComponents } from "./components/DeleteComponents";
-import { PostComponentFrom } from "./components/PostComponentFrom";
-import { TestComponents } from "./components/TestComponents";
-
 function App() {
   return (
     <>
-      <TestComponents />
-      <PostComponentFrom />
-      <DeleteComponents />
+      <p>Cao</p>
     </>
   );
 }
