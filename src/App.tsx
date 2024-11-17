7197ed1da8a5825a635036dd0c7704c72deb95ff
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2024-11-17 18:42:31 +0100

    desgin for login page is completed
    added background on login component and UI landing login page is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AppLayout } from "./components/AppLayout";
 import { Login } from "./components/login-components/Login";
 
 function App() {
   return (
-    <Router>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </AppLayout>
-    </Router>
+    <>
+      <Login />
+    </>
   );
 }
 
